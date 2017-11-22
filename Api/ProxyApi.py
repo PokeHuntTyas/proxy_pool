422eb72eb0505e4ca2abe5ba6e4c1f1b36095c60
PokeHuntTyas <33252470+PokeHuntTyas@users.noreply.github.com>
2017-11-22 17:31:36 +0100

    Update ProxyApi.py

--- Api/ProxyApi.py
+++ Api/ProxyApi.py
@@ -28,20 +28,38 @@ app = Flask(__name__)
 
 
 api_list = {
-    'get': u'get an usable proxy',
+    'get': u'get a usable proxy',
     # 'refresh': u'refresh proxy pool',
-    'get_all': u'get all proxy from proxy pool',
-    'delete?proxy=127.0.0.1:8080': u'delete an unable proxy',
+    'get_all': u'get all proxies from the proxy pool',
+    'delete?proxy=127.0.0.1:8080': u'delete a bad proxy',
     'get_status': u'proxy statistics'
 }
 
+def check_auth(username, password):
+   config = GetConfig()
+   return username == config.username and password = config.password
 
+def authenticate():
+    return Response(
+    'You\'re not allowed here.\n'
+    'Password not right', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 @app.route('/')
 def index():
     return jsonify(api_list)
 
 
 @app.route('/get/')
+@requires_auth
 def get():
     proxy = ProxyManager().get()
     return proxy if proxy else 'no proxy!'
@@ -56,12 +74,14 @@ def refresh():
 
 
 @app.route('/get_all/')
+@requires_auth
 def getAll():
     proxies = ProxyManager().getAll()
     return jsonify(proxies)
 
 
 @app.route('/delete/', methods=['GET'])
+@requires_auth
 def delete():
     proxy = request.args.get('proxy')
     ProxyManager().delete(proxy)
@@ -69,6 +89,7 @@ def delete():
 
 
 @app.route('/get_status/')
+@requires_auth
 def getStatus():
     status = ProxyManager().getNumber()
     return jsonify(status)
